846f8b3e9211473b9e9dd1ba35db41bbe81f3e63
lleaff <lleaff@users.noreply.github.com>
2015-03-10 09:55:47 +0100

    make tmp folder for vim swap files

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,4 +1,3 @@
-windowswindowh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
@@ -6,7 +5,7 @@ windowswindowh
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
+files=".bashrc .vimrc .vim .zshrc .oh-my-zsh"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -23,7 +22,10 @@ echo "...done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/$file
 done
+
+# create vim/tmp folder
+mkdir ~/.vim/tmp
