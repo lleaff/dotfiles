55edeb4ccf0021cb305c47c0d32a45a9cd43b0d0
lleaff <lleaff@users.noreply.github.com>
2015-03-11 04:10:44 +0100

    update

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -5,7 +5,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".xmodmaprc .bashrc .vimrc .vim .zshrc .oh-my-zsh"    # list of files/folders to symlink in homedir
+files=".bashrc .vimrc .vim .zshrc .oh-my-zsh"    # list of files/folders to symlink in homedir
 
 ##########
 
