a92ee006cd8438e59f2124a14ddd69cb49230254
lleaff <lleaff@users.noreply.github.com>
2015-03-15 20:05:38 +0100

    +.gitconfig ~formatting

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,11 +1,13 @@
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# This script creates symlinks from the home directory 
+#  to any desired dotfiles in ~/dotfiles
 ############################
 
 ########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/__old/dotfiles/             # old dotfiles backup directory
-files=".bashrc .vimrc .vim .zshrc .oh-my-zsh .gitconfig"    # list of files/folders to symlink in homedir
+# list of files/folders to symlink in homedir
+files=".bashrc .vimrc .vim .zshrc .oh-my-zsh .gitconfig"   
 
 ##########
 
@@ -19,7 +21,8 @@ echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# move any existing dotfiles in homedir to dotfiles_old
+#  directory, then create symlinks 
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/$file $olddir
