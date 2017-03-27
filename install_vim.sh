ca4e00542314fdc3df867a31dec1a0ef19b86c74
lleaff <lleaff@outlook.com>
2017-03-27 12:33:37 +0200

    vim: Update install_vim.sh

--- install_vim.sh
+++ install_vim.sh
@@ -1,22 +1,43 @@
 #!/bin/bash
 set -o nounset  # Treat unset variables as an error
 
+STARTING_DIR="$PWD"
 
-#echo "Installing Dein...
-#________________________"
-## Unix
-#DEIN_INSTALLATION_DIRECTORY=$HOME'/dotfiles/.vim/bundle/dein.vim'
-#
-#DEIN_INSTALLER=$HOME'/dein_install.sh~'
-#curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > $DEIN_INSTALLER
-#sh $DEIN_INSTALLER $DEIN_INSTALLATION_DIRECTORY
+#------------------------------------------------------------
 
 echo "Installing vim-plug...
 ______________________"
 
+VIM_DIR="$HOME/.vim"
+PLUGINS_DIR="${VIM_DIR}/plugged"
+
 set -e # Stop on error
 # Unix
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLo "${VIM_DIR}/autoload/plug.vim" --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +'PlugInstall' +':qa'
-echo "Done! Use :PlugInstall command to update plugins."
+echo "Done installing vim-plug. Use :PlugInstall command to update plugins."
+
+#------------------------------------------------------------
+
+echo "Building vimproc...
+___________________"
+cd "${PLUGINS_DIR}/vimproc.vim/"
+make
+
+#------------------------------------------------------------
+
+echo "Installing tern_for_vim dependencies...
+_______________________________________"
+cd "${PLUGINS_DIR}/tern_for_vim/"
+if hash yarn 1>/dev/null 2>&1; then
+  yarn install
+elif hash npm 1>/dev/null 2>&1; then
+  npm install
+else
+  echo "Install npm and execute 'npm install' in '${PLUGINS_DIR}/tern_for_vim/' to install required packages for tern_for_vim."
+fi
+
+echo "Done!"
+
+cd "$STARTING_DIR"
